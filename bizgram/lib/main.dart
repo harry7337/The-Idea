3f89f71de8d1d7c70d3eb4b68a5ae41abb67108a
varun290101 <73779567+varun290101@users.noreply.github.com>
2021-08-24 09:47:50 +0530

    SignIn with google created

--- bizgram/lib/main.dart
+++ bizgram/lib/main.dart
@@ -1,27 +1,95 @@
-import 'package:flutter/material.dart';
+import 'package:bizgram/screen/authenticate/signin.dart';
+import 'package:flutter/material.dart' ;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'constants/UIconstants.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  @override 
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Bizgram',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sign Up Page'),
-        ),
-        body: Center(
-          child: Text(
-            'You have pushed the button this many times:',
-          ),
-        ),
-      ),
+      title: 'bizgram',
+      home: Home(),
     );
+  }
+}
+
+class Home extends StatefulWidget {
+  
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Color primary= Color.fromRGBO(245, 245, 220, 20);
+  Color secondary = Color.fromRGBO(255, 218, 185, 20);
+  Color logo = Color.fromRGBO(128,117,90,60);
+  @override
+  
+
+  Widget build(BuildContext context) {
+    return new Scaffold(
+         body: Container(
+           height: UIConstants.fitToHeight(640, context),
+          width: UIConstants.fitToWidth(360, context),
+           padding: EdgeInsets.symmetric(horizontal: 20),
+           child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    //We take the image from the assets
+                    Image.asset(
+                      'images/img1.png',
+                      height: UIConstants.fitToHeight(240, context),
+                      width: UIConstants.fitToWidth(300, context),
+                    ),
+                    SizedBox(
+                      height: UIConstants.fitToHeight(20, context),
+                    ),
+                    //Texts and Styling of them
+                    Text(
+                      'Welcome!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black, fontSize: 28),
+                    ),
+                    SizedBox(height: UIConstants.fitToHeight(20, context)),
+                    Text(
+                      'There is always a start!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: UIConstants.fitToHeight(20, context),
+                    ),
+                    //Our MaterialButton which when pressed will take us to a new screen named as 
+                    //LoginScreen
+                    MaterialButton(
+                      elevation: 0,
+                      height: UIConstants.fitToHeight(50, context),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => LoginScreen()));
+                      },
+                      color: logo,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Get Started',
+                              style: TextStyle(color: Colors.white, fontSize: 20)),
+                          Icon(Icons.arrow_forward_ios)
+                        ],
+                      ),
+                      textColor: Colors.white,
+                    )
+                  ],
+                ),
+         ),
+      
+    );
+      
   }
 }
--- bizgram/lib/screen/authenticate/signin.dart
+++ bizgram/lib/screen/authenticate/signin.dart
@@ -1,130 +1,146 @@
+import 'package:bizgram/constants/UIconstants.dart';
 import 'package:flutter/material.dart';
-import 'package:huegahsample/services/auth.dart';
-import 'package:huegahsample/screen/wrapper.dart';
-import 'package:huegahsample/shared/constants.dart';
-import 'package:huegahsample/shared/loading.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatefulWidget {
-  final Function toggleViewParameter;
-  SignIn({this.toggleViewParameter});
+
+class LoginScreen extends StatefulWidget {
+
   @override
-  _SignInState createState() => _SignInState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _SignInState extends State<SignIn> {
-  final AuthService _auth = AuthService();
-  final _formKey = GlobalKey<FormState>();
-  bool loading = false;
-  dynamic result;
+class _LoginScreenState extends State<LoginScreen> {
+  Color primary= Color.fromRGBO(245, 245, 220, 20);
+  Color secondary = Color.fromRGBO(255, 218, 185, 20);
+  Color logo = Color.fromRGBO(128,117,90,60);
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final FirebaseAuth firebaseAuth= FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
-  //text field state
-  String email = '';
-  String pass = '';
-  String error = '';
   @override
-  Widget build(BuildContext context) {
-    return loading
-        ? Loading()
-        : Scaffold(
-            backgroundColor: Colors.brown[100],
-            appBar: AppBar(
-              backgroundColor: Colors.brown[400],
-              elevation: 0.0,
-              title: Text('Sign In to Huegah'),
-              actions: [
-                FlatButton.icon(
-                  onPressed: () {
-                    widget.toggleViewParameter();
-                  },
-                  icon: Icon(Icons.person),
-                  label: Text('Sign Up'),
+   Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        backgroundColor: primary,
+        body: Container(
+          alignment: Alignment.topCenter,
+          margin: EdgeInsets.symmetric(horizontal: 30),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign in and support small businesses!',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.black, fontSize: 28),
                 ),
+                SizedBox(height: UIConstants.fitToHeight(20, context)),
+                Text(
+                  'Enter your email and password below',
+                  textAlign: TextAlign.center,
+                  style:
+                      GoogleFonts.openSans(color: Colors.black, fontSize: 14),
+                ),
+                SizedBox(
+                  height: UIConstants.fitToHeight(50, context),
+                ),
+                _buildTextField(
+                    nameController, Icons.account_circle, 'Username'),
+                SizedBox(height: UIConstants.fitToHeight(20, context)),
+                _buildTextField(passwordController, Icons.lock, 'Password'),
+                SizedBox(height: UIConstants.fitToHeight(30, context)),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: UIConstants.fitToHeight(50, context),
+                  onPressed: () {},
+                  color: logo,
+                  child: Text('Login',
+                      style: TextStyle(color: Colors.black, fontSize: 16)),
+                  textColor: Colors.black,
+                ),
+                SizedBox(height: UIConstants.fitToHeight(20, context)),
+                MaterialButton(
+                  elevation: 0,
+                  minWidth: double.maxFinite,
+                  height: UIConstants.fitToHeight(50, context),
+                  onPressed: ()  async {
+                   
+                final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+                if(googleUser != null){
+                final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+                
+                final AuthCredential credential = GoogleAuthProvider.credential(
+                  idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+                  final User? user = (await firebaseAuth.signInWithCredential(credential)).user;
+                }}
+                   ,
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(FontAwesomeIcons.google),
+                      SizedBox(width: 10),
+                      Text('Sign-in using Google',
+                          style: TextStyle(color: Colors.black, fontSize: 16)),
+                    ],
+                  ),
+                  textColor: Colors.black,
+                ),
+                SizedBox(height: UIConstants.fitToHeight(100, context)),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: _buildFooterLogo(),
+                )
               ],
             ),
-            body: Container(
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration: textInputDecoration,
-                        validator: (val) =>
-                            val.isEmpty ? 'Enter an email' : null,
-                        onChanged: (val) {
-                          setState(() {
-                            email = val;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        decoration:
-                            textInputDecoration.copyWith(hintText: 'Password'),
-                        validator: (val) => val.length < 6
-                            ? 'Enter a password longer than 6 characters'
-                            : null,
-                        obscureText: true,
-                        onChanged: (val) {
-                          setState(() {
-                            pass = val;
-                          });
-                        },
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      RaisedButton(
-                        onPressed: () async {
-                          if (_formKey.currentState.validate()) {
-                            setState(() {
-                              loading = true;
-                            });
-                            result = await _auth.signInWithEmailAndPassword(
-                                email, pass);
-                            if (result == null) {
-                              setState(() {
-                                loading = false;
-                                error = 'Could Not Sign In Those Credentials';
-                              });
-                            } else {
-                              setState(() {
-                                loading = false;
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => Wrapper(),
-                                  ),
-                                );
-                                print("Signed in!");
-                              });
-                            }
-                          }
-                        },
-                        color: Colors.pink[400],
-                        child: Text(
-                          'Sign In',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 12,
-                      ),
-                      Text(
-                        error,
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  )),
+          ),
+        ));
+  }
+
+  _buildFooterLogo() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('BiZgram',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.openSans(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+ 
+  _buildTextField(
+      TextEditingController controller, IconData icon, String labelText) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          color: secondary, border: Border.all(color: Colors.blue)),
+      child: TextField(
+        controller: controller,
+        style: TextStyle(color: Colors.black),
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(horizontal: 10),
+            labelText: labelText,
+            labelStyle: TextStyle(color: Colors.black),
+            icon: Icon(
+              icon,
+              color: Colors.black,
             ),
-          );
+            // prefix: Icon(icon),
+            border: InputBorder.none),
+      ),
+    );
   }
 }
